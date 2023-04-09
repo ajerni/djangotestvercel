8ce01633ee09f164fdb0599b60522413fbd2f497
„Andi <ajerni@gmail.com>
2023-04-09 13:10:33 +0200

    about page added

--- webpage/views.py
+++ webpage/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 def home(request):
     name = 'Andi'
     return render(request, 'home.html', {'name':name})
+
+def about(request):
+    return render(request, 'about.html', {})
